4963f881c821897fa1c4f33323b3b76021586f86
Wenli Fei <viviuuuli@gmail.com>
2025-10-06 20:47:24 -0400

    feat: add helper methods for parsing event details

--- src/main/java/rpc/SearchItem.java
+++ src/main/java/rpc/SearchItem.java
@@ -1,24 +1,45 @@
 package rpc;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import entity.Item;
+import external.TicketMasterAPI;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet(name = "SearchItem", urlPatterns = {"/search"})
 public class SearchItem extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        JSONArray arr = new JSONArray()
-                .put(new JSONObject().put("username", "abcd"))
-                .put(new JSONObject().put("username", "1234"));
-        RpcHelper.writeJsonArray(resp, arr);
+//        JSONArray arr = new JSONArray()
+//               .put(new JSONObject().put("username", "abcd"))
+//                .put(new JSONObject().put("username", "1234"));
+//        RpcHelper.writeJsonArray(resp, arr);
+        JSONArray array = new JSONArray();
+        try {
+            double lat = Double.parseDouble(request.getParameter("lat"));
+            double lon = Double.parseDouble(request.getParameter("lon"));
+            String keyword = request.getParameter("term");
+
+            TicketMasterAPI tmAPI = new TicketMasterAPI();
+            List<Item> items = tmAPI.search(lat, lon, keyword);
+
+            for (Item item : items) {
+                JSONObject obj = item.toJSONObject();
+                array.put(obj);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        RpcHelper.writeJsonArray(response, array);
+
     }
 
     @Override
